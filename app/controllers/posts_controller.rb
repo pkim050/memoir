0960bf8032fad38ba790ea72ac167d037f215649
pkim050 <pkim@gmail.com>
2023-02-08 14:21:51 -0800

    Problem A solution(s).

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,19 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = current_user.posts
   end
 
   # GET /posts/1 or /posts/1.json
   def show
+    @post = Post.find_by(id: params[:id])
+    if current_user && current_user.id == @post.user.id
+      @post
+    else
+      flash.now[:alert] = "Post #{@post.id} not found!"
+      @posts = current_user.posts
+      render "index"
+    end
   end
 
   # GET /posts/new
@@ -19,6 +27,14 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
+    @post = Post.find_by(id: params[:id])
+    if current_user && current_user.id == @post.user.id
+      @post
+    else
+      flash.now[:alert] = "Post #{@post.id} not found!"
+      @posts = current_user.posts
+      render "index"
+    end
   end
 
   # POST /posts or /posts.json
@@ -28,7 +44,7 @@ class PostsController < ApplicationController
     @post.user = current_user
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to user_post_path(current_user, @post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,8 +57,8 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
+        format.html { redirect_to user_post_path(current_user, @post), notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: user_post_path }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -54,7 +70,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to user_posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
     end
   end
